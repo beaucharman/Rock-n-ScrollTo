7a6e04c905162ea929e04674520674eb6429f5ca
Beau Charman <beau.charman@gmail.com>
2013-01-28 12:49:08 +1100

    Changes to the description

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Rock 'n ScrollTo
+
+A small jQuery plugin to help get users from one place to another.
+
+## Usage
+$('document').ready(function()
+{
+  /* fire! */
+  $('button').on('click', function()
+  { 
+    $('#section').rockNScrollTo(800, 60, function()
+    {
+      /* stuff to run after rockNScrollTo has finished it's business. */
+    });
+    return false; /* stop anchor from performing it's default action. */
+  });
+});
---
+++ rock-n-scrollto.js
@@ -0,0 +1,24 @@
+/* 
+
+  Rock 'n ScrollTo
+
+------------------------------------------------
+  rock-n-scrollto.js 1.0 
+  Monday, 28th January 2013
+  Beau Charman | @beaucharman | http://beaucharman.me
+------------------------------------------------ */
+
+$.fn.extend(
+{
+  rockNScrollTo : function(speed, offset, callback)
+  {
+    speed  = speed      || 400;
+    offset = offset     || 0;
+    callback = callback || null;
+    return this.each(function()
+    {
+      var targetOffset = $(this).offset().top - offset;
+      $('body').animate({scrollTop: targetOffset}, speed, callback);
+    });
+  }
+});
