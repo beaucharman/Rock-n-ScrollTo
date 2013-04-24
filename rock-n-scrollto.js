57bf4f03ab417158a1687a4c875a8875b5f1238e
Beau <beau.charman@gmail.com>
2013-04-24 13:33:56 +1000

    Update rock-n-scrollto.js

--- rock-n-scrollto.js
+++ rock-n-scrollto.js
@@ -1,22 +1,23 @@
-/* 
+/** 
+ * Rock 'n ScrollTo
+ * ------------------------------------------------------------------------
+ * rock-n-scrollto.js
+ * @version 1.0 | Monday, 28th January 2013
+ * @author  Beau Charman | @beaucharman | http://beaucharman.me
+ * @link    https://gist.github.com/beaucharman/5127116/
+ * @param array      | array
+ * @param callback   | function
+ * @return callback   | function
+ * 
+ * Iterate through an object, array, string etc...
+ * ------------------------------------------------------------------------ */
 
-  Rock 'n ScrollTo
-
-------------------------------------------------
-  rock-n-scrollto.js 1.0 
-  Monday, 28th January 2013
-  Beau Charman | @beaucharman | http://beaucharman.me
------------------------------------------------- */
-
-$.fn.extend(
-{
-  rockNScrollTo : function(speed, offset, callback)
-  {
-    speed  = speed      || 400;
+$.fn.extend({
+  rockNScrollTo : function(speed, offset, callback) {
+    speed = speed       || 400;
     offset = offset     || 0;
     callback = callback || null;
-    return this.each(function()
-    {
+    return this.each(function() {
       var targetOffset = $(this).offset().top - offset;
       $('body').animate({scrollTop: targetOffset}, speed, callback);
     });
