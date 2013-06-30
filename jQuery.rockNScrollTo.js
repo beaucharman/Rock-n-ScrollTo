74ec824cf329593d59724d648b920cdc85a02885
Beau Charman <beau.charman@gmail.com>
2013-06-30 23:49:30 +1000

    Comment changes

--- jQuery.rockNScrollTo.js
+++ jQuery.rockNScrollTo.js
@@ -2,15 +2,15 @@
  * rockNScrollTo.js
  * ========================================================================
  * jquery.rockNScrollTo.js
- * @version 2.0 | Monday, 14th May 2013
- * @author  Beau Charman | @beaucharman | http://www.beaucharman.me
- * @link    https://github.com/beaucharman/rockNScrollTo.js
- * @license MIT license
- * @param {object} options
- *          {integer}  offset
- *          {integer}  speed
- *          {string}   easing
- *          {function} callback
+ * @version   2.0 | 30 June 2013
+ * @author    Beau Charman | @beaucharman | http://www.beaucharman.me
+ * @link      https://github.com/beaucharman/rockNScrollTo.js
+ * @license   MIT license
+ * @param     {object} options
+ *              {integer}  offset
+ *              {integer}  speed
+ *              {string}   easing
+ *              {function} callback
  *
  * Scroll from the currrent window state to a given element's top offest
  * ======================================================================== */
